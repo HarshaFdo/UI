58ddf821eef13f730ec41f2f7311d8636f3faeb6
Harsha Fernando <fernando.harsha2016@gmail.com.com>
2025-10-25 00:16:22 +0530

    add upload box

--- src/app/vehicles/vehicles.component.ts
+++ src/app/vehicles/vehicles.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { vehicle } from './Vehilces.model';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { FormsModule } from '@angular/forms';
+import { FileUpload, FileUploadModule } from 'primeng/fileupload';
+import { InputTextModule } from 'primeng/inputtext';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-vehicles',
@@ -15,19 +18,37 @@ import { FormsModule } from '@angular/forms';
     ButtonModule,
     InputNumberModule,
     FormsModule,
+    FileUploadModule,
+    InputTextModule,
+    HttpClientModule,
   ],
   templateUrl: './vehicles.component.html',
   styleUrls: ['./vehicles.component.scss'],
 })
 export class VehiclesComponent implements OnInit {
+  @ViewChild('fileUploader') fileUploader!: FileUpload;
+
   inputValue: number = 0;
   vehicles: vehicle[] = [];
-
-  constructor() {}
+  uploadedFiles: any[] = [];
 
   ngOnInit(): void {}
 
   submitValue(): void {
     console.log('Submitted value:', this.inputValue);
+  }
+
+  openFileUploader() {
+    if (this.fileUploader) {
+      this.fileUploader.choose(); // opens the file dialog
+    }
+  }
+
+  onUpload(event: any) {
+    for (const file of event.files) {
+      this.uploadedFiles.push(file);
+    }
+
+    console.log('Upload successfull!\n Files: ', this.uploadedFiles);
   }
 }
